96c73767b38349f25757ac8c1f4e2cfce54456dd
Aviv Maman <awiwmaman@outlook.com>
2023-04-20 14:57:34 +0300

    clean

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import { Inter } from 'next/font/google';
-import HelloBlock from '@/components/HelloBlock';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
   return (
-    <main className={inter.className}>
-      <div className='grid'>
-        <HelloBlock />
-        <HelloBlock />
-        <HelloBlock />
-      </div>
-    </main>
+    <div>
+      <p>Home</p>
+    </div>
   );
 }
