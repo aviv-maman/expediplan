e8f0a2b1e6ea390c4e69033216a72e0f99d5f481
Aviv Maman <awiwmaman@outlook.com>
2023-04-21 23:06:09 +0300

    Cities

--- src/app/api/cities/route.ts
+++ src/app/api/cities/route.ts
@@ -1,11 +1,11 @@
 import { NextResponse } from 'next/server';
-import dataCities from '@/data.json';
+import cities from '@/compactCities.json';
 
 export async function GET(request: Request) {
   const { search } = new URL(request.url);
   const searchParams = new URLSearchParams(search);
   const searchValue = searchParams.get('id');
   const ids = searchValue?.split(',') || [];
-  const filteredCities = dataCities.filter((city, index) => city.id === Number(ids[index]));
-  return NextResponse.json(ids);
+  const filteredCities = cities.filter((city, index) => city.id === Number(ids[index]));
+  return NextResponse.json(filteredCities);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,13 @@ import 'server-only';
 import CustomStack from '@/components/home/CustomStack';
 import Features from '@/components/home/Features/Features';
 import Hero from '@/components/home/Hero';
+import CarouselCities from '@/components/home/CarouselCities';
 
 async function getCities(): Promise<any> {
   const env = process.env.NODE_ENV;
   const hostname = env === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_HOSTNAME;
   // const params = new URLSearchParams({ id: ['1,2,77340,143446'] });
-  const ids = ['4', '2'];
+  const ids = ['77340', '143446', '59582', '44856', '50388', '99972'];
   const params = new URLSearchParams({ id: String(ids) });
   const res = await fetch(`${hostname}/api/cities?${params}`, {
     cache: 'default',
@@ -38,6 +39,7 @@ export default async function Home() {
     <CustomStack>
       <Hero />
       <Features />
+      <CarouselCities />
       {JSON.stringify(cities)}
     </CustomStack>
   );
