a6f006988a6379d4205d906a4a137b15fdb645a4
Aviv Maman <awiwmaman@outlook.com>
2023-04-21 18:17:24 +0300

    Start of cities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,44 @@
+import 'server-only';
 import CustomStack from '@/components/home/CustomStack';
 import Features from '@/components/home/Features/Features';
 import Hero from '@/components/home/Hero';
 
-export default function Home() {
+async function getCities(): Promise<any> {
+  const env = process.env.NODE_ENV;
+  const hostname = env === 'development' ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_HOSTNAME;
+  // const params = new URLSearchParams({ id: ['1,2,77340,143446'] });
+  const ids = ['4', '2'];
+  const params = new URLSearchParams({ id: String(ids) });
+  const res = await fetch(`${hostname}/api/cities?${params}`, {
+    cache: 'default',
+  });
+  if (!res.ok) {
+    // This will activate the closest `error.tsx` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+  const data = await res.json();
+  if (data.message) {
+    throw new Error(data.message);
+  }
+  return Object.values(data);
+}
+
+// const getCurrentTime = async (): Promise<string> => {
+//   const res = await fetch('https://worldtimeapi.org/api/timezone/America/Chicago', {
+//     cache: 'no-store',
+//   });
+//   const data = await res.json();
+//   return data.datetime;
+// };
+
+export default async function Home() {
+  const [cities] = await Promise.all([getCities()]);
+
   return (
     <CustomStack>
       <Hero />
       <Features />
+      {JSON.stringify(cities)}
     </CustomStack>
   );
 }
