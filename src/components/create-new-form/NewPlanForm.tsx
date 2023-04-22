9bb5cc920355e63357a5e86f733af26b1d99201a
Aviv Maman <awiwmaman@outlook.com>
2023-04-23 02:52:15 +0300

    Start of showing flag

--- src/components/create-new-form/NewPlanForm.tsx
+++ src/components/create-new-form/NewPlanForm.tsx
@@ -1,10 +1,33 @@
 'use client';
-import { Box, Button, Group, Paper, Select, TextInput, Title } from '@mantine/core';
+import { Avatar, Box, Button, Group, Paper, Select, TextInput, ThemeIcon, Title, createStyles, rem } from '@mantine/core';
 import { hasLength, isNotEmpty, useForm } from '@mantine/form';
-import { IconFlag } from '@tabler/icons-react';
+import { IconFlag, IconFlagFilled } from '@tabler/icons-react';
 import DropdownWithIcon from '../DropdownWithIcon';
 import type { City, Country } from '../../../types/general';
 import useSWR from 'swr';
+import { DatePickerInput } from '@mantine/dates';
+
+const ICON_SIZE = rem(60);
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    position: 'relative',
+    overflow: 'visible',
+    padding: theme.spacing.xl,
+    paddingTop: `calc(${theme.spacing.xl} * 1.5 + ${ICON_SIZE} / 3)`,
+  },
+
+  icon: {
+    position: 'absolute',
+    top: `calc(-${ICON_SIZE} / 3)`,
+    left: `calc(50% - ${ICON_SIZE} / 2)`,
+  },
+
+  title: {
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1,
+  },
+}));
 
 const countriesFetcher = (url: string) => fetch(url).then((res) => res.json() as Promise<Country[]>);
 const getCountriesAPI = (ids?: string | string[]) => {
@@ -24,11 +47,16 @@ const getCityByCountryIdAPI = (id: number) => {
 };
 
 const NewPlanForm: React.FC = () => {
+  const { classes } = useStyles();
+
   const form = useForm({
     initialValues: {
       name: '',
       country: 0,
       city: 0,
+      startDate: undefined,
+      endDate: undefined,
+      flag: '',
     },
     validate: {
       name: isNotEmpty('Enter a name for your plan') && hasLength({ min: 3 }, 'Name must have 3 or more characters'),
@@ -48,7 +76,13 @@ const NewPlanForm: React.FC = () => {
       <Title align='center' sx={(theme) => ({ fontWeight: 900 })}>
         Create New Plan
       </Title>
-      <Paper withBorder shadow='md' p={30} mt={30} radius='md'>
+      <Paper withBorder shadow='md' p={30} radius='md' className={classes.card} mt={`calc(${ICON_SIZE} / 3)`}>
+        {/* <ThemeIcon className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE}>
+          <IconFlagFilled size='2rem' stroke={1.5} />
+        </ThemeIcon> */}
+        <Avatar src={form.values.flag} alt='flag' className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE} color='blue'>
+          <IconFlagFilled size='2rem' stroke={1.5} />
+        </Avatar>
         <form
           onSubmit={form.onSubmit((values) => {
             values.country = Number(values.country);
@@ -73,6 +107,11 @@ const NewPlanForm: React.FC = () => {
             {...form.getInputProps('country')}
             transitionProps={{ transition: 'pop-top-left', duration: 80, timingFunction: 'ease' }}
             disabled={!countries.data || countries.isLoading}
+            // onChange={(value) => {
+            //   form.setFieldValue('country', Number(value));
+            //   form.setFieldValue('flag', countries.data?.find((item) => item.id === Number(value))?.flag || '');
+            //   form.setFieldValue('city', 0);
+            // }}
           />
           <Select
             required
@@ -87,6 +126,8 @@ const NewPlanForm: React.FC = () => {
             disabled={form.values.country === 0 || !cities.data || cities.isLoading}
             {...form.getInputProps('city')}
           />
+          <DatePickerInput label='Start Date' placeholder='Choose start date' mx='auto' maw={400} {...form.getInputProps('startDate')} />
+          <DatePickerInput label='End Date' placeholder='Choose end date' mx='auto' maw={400} {...form.getInputProps('endDate')} />
           <Group position='right' mt='md'>
             <Button type='submit'>Create</Button>
           </Group>
