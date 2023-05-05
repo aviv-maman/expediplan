3d71793166c76bac33d6b8ebffc24ef6cdb080aa
Aviv Maman <awiwmaman@outlook.com>
2023-05-05 20:06:46 +0300

    TypeScript ignoreBuildErrors

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,51 +1,30 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
-import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
-  return NextAuth({
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string,
-        // profile(profile) {
-        //   return {
-        //     id: profile.id,
-        //     name: profile.name,
-        //     email: profile.email,
-        //     image: profile.avatar_url,
-        //   };
-        // },
-      }),
-    ],
-    callbacks: {
-      async session({ session, token, user }) {
-        if (session.user) {
-          session.user.id = token.sub;
-        }
+const handler: NextAuthOptions = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+      // profile(profile) {
+      //   return {
+      //     id: profile.id,
+      //     name: profile.name,
+      //     email: profile.email,
+      //     image: profile.avatar_url,
+      //   };
+      // },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token, user }) {
+      if (session.user) {
+        session.user.id = token.sub;
+      }
 
-        return session;
-      },
+      return session;
     },
-  });
-}
+  },
+});
 
-export async function POST(request: NextRequest) {
-  return NextAuth({
-    providers: [
-      GithubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string,
-      }),
-    ],
-    callbacks: {
-      async session({ session, token }) {
-        if (session.user) {
-          session.user.id = token.sub;
-        }
-
-        return session;
-      },
-    },
-  });
-}
+export { handler as GET, handler as POST };
