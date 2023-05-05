760310f41a0d952635d670a340a0bc044bb6cee0
Aviv Maman <awiwmaman@outlook.com>
2023-05-05 19:53:11 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,46 +1,51 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import type { NextRequest } from 'next/server';
 
-export const GET: NextAuthOptions = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-      // profile(profile) {
-      //   return {
-      //     id: profile.id,
-      //     name: profile.name,
-      //     email: profile.email,
-      //     image: profile.avatar_url,
-      //   };
-      // },
-    }),
-  ],
-  callbacks: {
-    async session({ session, token, user }) {
-      if (session.user) {
-        session.user.id = token.sub;
-      }
+export async function GET(request: NextRequest) {
+  return NextAuth({
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+        // profile(profile) {
+        //   return {
+        //     id: profile.id,
+        //     name: profile.name,
+        //     email: profile.email,
+        //     image: profile.avatar_url,
+        //   };
+        // },
+      }),
+    ],
+    callbacks: {
+      async session({ session, token, user }) {
+        if (session.user) {
+          session.user.id = token.sub;
+        }
 
-      return session;
+        return session;
+      },
     },
-  },
-});
+  });
+}
 
-export const POST: NextAuthOptions = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-  callbacks: {
-    async session({ session, token }) {
-      if (session.user) {
-        session.user.id = token.sub;
-      }
+export async function POST(request: NextRequest) {
+  return NextAuth({
+    providers: [
+      GithubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+    ],
+    callbacks: {
+      async session({ session, token }) {
+        if (session.user) {
+          session.user.id = token.sub;
+        }
 
-      return session;
+        return session;
+      },
     },
-  },
-});
+  });
+}
