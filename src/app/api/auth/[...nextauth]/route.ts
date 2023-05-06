5d9d22ee9770c8da22ce2dda8d6a60f06ea0e67b
Aviv Maman <awiwmaman@outlook.com>
2023-05-06 16:36:17 +0300

    CHG: Authentication is temporarily  disabled

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,36 @@
-import NextAuth, { type NextAuthOptions } from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+// import NextAuth, { type NextAuthOptions } from 'next-auth';
+// import GithubProvider from 'next-auth/providers/github';
 
-const handler: NextAuthOptions = NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-      // profile(profile) {
-      //   return {
-      //     id: profile.id,
-      //     name: profile.name,
-      //     email: profile.email,
-      //     image: profile.avatar_url,
-      //   };
-      // },
-    }),
-  ],
-  callbacks: {
-    async session({ session, token, user }) {
-      if (session.user) {
-        session.user.id = token.sub;
-      }
+// const handler: NextAuthOptions = NextAuth({
+//   providers: [
+//     GithubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string,
+//       // profile(profile) {
+//       //   return {
+//       //     id: profile.id,
+//       //     name: profile.name,
+//       //     email: profile.email,
+//       //     image: profile.avatar_url,
+//       //   };
+//       // },
+//     }),
+//   ],
+//   callbacks: {
+//     async session({ session, token, user }) {
+//       if (session.user) {
+//         session.user.id = token.sub;
+//       }
 
-      return session;
-    },
-  },
-});
+//       return session;
+//     },
+//   },
+// });
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
+
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  return NextResponse.json({ message: 'Hello world' });
+}
