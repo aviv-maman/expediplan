ded00b0caed8f6361b11431c3bcfa173cb0847f6
Aviv Maman <awiwmaman@outlook.com>
2023-05-12 01:47:28 +0300

    Update route.ts

--- src/app/api/attractions/city/[id]/route.ts
+++ src/app/api/attractions/city/[id]/route.ts
@@ -1,20 +1,21 @@
 import { NextRequest, NextResponse } from 'next/server';
 import attractions from '@/attractions.json';
-import { Attraction } from '../../../../../../types/general';
+import type { Attraction } from '../../../../../../types/general';
+import { CategoryName } from '@/constants';
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
   const cityId = Number(params.id);
   const { search } = new URL(request.url);
   const searchParams = new URLSearchParams(search);
   const searchValue = searchParams.get('category');
-  const categories = searchValue?.endsWith(',') ? searchValue.slice(0, -1).split(',') : searchValue?.split(',');
+  const categories = searchValue?.split(',') as CategoryName[] | undefined;
 
   if (!categories || categories.length === 0) {
-    const filteredAttractions: Attraction[] = attractions.filter((attraction) => attraction.city === cityId);
+    const filteredAttractions = attractions.filter((attraction) => attraction.city === cityId) as Attraction[];
     return NextResponse.json(filteredAttractions);
   }
-  const filteredAttractions: Attraction[] = attractions.filter((attraction) =>
+  const filteredAttractions = attractions.filter((attraction) =>
     categories?.some((category) => decodeURIComponent(attraction.category) === category && attraction.city === cityId)
-  );
+  ) as Attraction[];
   return NextResponse.json(filteredAttractions);
 }
