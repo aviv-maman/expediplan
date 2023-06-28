b9aef6549f18e134770a4d7b1d133c69491d9079
Aviv Maman <awiwmaman@outlook.com>
2023-06-29 00:24:50 +0300

    Update EditPlanForm.tsx

--- src/components/plans/EditPlanForm.tsx
+++ src/components/plans/EditPlanForm.tsx
@@ -1,26 +1,13 @@
 'use client';
-import { Avatar, Box, Button, Group, Paper, Select, TextInput, Title, createStyles, rem } from '@mantine/core';
+import { Box, Button, Group, Paper, TextInput, Title, createStyles, rem } from '@mantine/core';
 import { hasLength, isNotEmpty, useForm } from '@mantine/form';
-import { IconBuilding, IconCalendar, IconFlag, IconFlagFilled, IconId } from '@tabler/icons-react';
-import DropdownWithIcon from '../DropdownWithIcon';
-import type { Plan } from '../../../types/general';
+import { IconId } from '@tabler/icons-react';
 import useSWR from 'swr';
-import { DatePickerInput } from '@mantine/dates';
-import dayjs from 'dayjs';
-import { Suspense, useEffect, useState } from 'react';
+import { useState } from 'react';
 import { countriesFetcher, getCountriesAPI } from '@/api/CountriesAPI';
-import { citiesFetcher, cityFetcher, getCitiesByCountryIdAPI, getCityByIdAPI } from '@/api/CitiesAPI';
-import { useParams, useSearchParams } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import {
-  editPlanOnLocalStorage,
-  editPlanOnServer,
-  getPlanByIdAPI,
-  getPlanByIdFromLocalStorage,
-  planFetcher,
-  savePlanToLocalStorage,
-  uploadPlanToServer,
-} from '@/api/PlansAPI';
+import { editPlanOnLocalStorage, editPlanOnServer, getPlanByIdAPI, getPlanByIdFromLocalStorage, planFetcher } from '@/api/PlansAPI';
 
 const ICON_SIZE = rem(60);
 
@@ -57,58 +44,24 @@ const EditPlanForm: React.FC<EditPlanFormProps> = ({ closeModal }) => {
     suspense: true,
   });
   const plan = planFromServer ? planFromServer : getPlanByIdFromLocalStorage(planId);
-  const city = useSWR(getCityByIdAPI(Number(plan?.city)), cityFetcher, { suspense: true });
   const [isLoading, setIsLoading] = useState(false);
 
   const form = useForm({
     initialValues: {
       name: plan?.name || '',
-      // country: { id: '', flag: '' },
-      // city: '',
-      // startDate: undefined,
-      // endDate: undefined,
-      // countryName: '',
-      // cityName: '',
     },
     validate: {
       name: isNotEmpty('Enter a name for your plan') && hasLength({ min: 3 }, 'Name must have 3 or more characters'),
-      // country: isNotEmpty('Country is required'),
-      // city: isNotEmpty('City is required'),
     },
 
     transformValues: (values) => ({
       ...values,
-      // country: Number(values.country.id) || 0,
-      // city: Number(values.city) || 0,
-      // startDate: new Date(dayjs(values.startDate).format('YYYY-MM-DD') || 0), //dayjs(values.startDate).format('YYYY-MM-DD')
-      // endDate: new Date(dayjs(values.endDate).format('YYYY-MM-DD') || 0), //values.endDate.toISOString().split('T')[0]
     }),
   });
 
-  const searchParams = useSearchParams();
-
   const countries = useSWR(getCountriesAPI(), countriesFetcher);
-  // const cities = useSWR(getCitiesByCountryIdAPI(form.getTransformedValues().country), citiesFetcher);
-
-  // const duration = dayjs(form.values.endDate).diff(dayjs(form.values.startDate), 'day') + 1;
-
-  //===[ Create new plan from search params ]===
-  // const flag = countries.data?.find((item) => item.id === Number(form.values.country.id))?.flag;
-  useEffect(() => {
-    if (searchParams.get('country')) {
-      form.setFieldValue('country.id', searchParams.get('country') || '');
-    }
-    if (searchParams.get('city')) {
-      form.setFieldValue('city', searchParams.get('city') || '');
-    }
-  }, []);
-  //===========================================
 
   if (countries.error) return <div>Failed to load</div>;
-  // if (cities.error) return <div>Failed to load</div>;
-
-  // if (countries.isLoading) return <div>Loading...</div>;
-  // if (cities.isLoading) return <div>Loading...</div>;
 
   return (
     <Box maw={300} mx='auto'>
@@ -116,9 +69,6 @@ const EditPlanForm: React.FC<EditPlanFormProps> = ({ closeModal }) => {
         Edit Plan
       </Title>
       <Paper withBorder shadow='md' p={30} radius='md' className={classes.card} mt={`calc(${ICON_SIZE} / 3)`}>
-        {/* <Avatar src={form.values.country.flag || flag} alt='flag' className={classes.icon} size={ICON_SIZE} radius={ICON_SIZE} color='blue'>
-          <IconFlagFilled size='2rem' stroke={1.5} />
-        </Avatar> */}
         <form
           onSubmit={form.onSubmit(async (values) => {
             setIsLoading(true);
